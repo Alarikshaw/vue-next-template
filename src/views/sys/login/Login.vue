dfe787324526ddfd1fef24878b35847923096e4e
alarikshaw <337349132@qq.com>
2020-11-27 10:21:31 +0800

    vscode

--- src/views/sys/login/Login.vue
+++ src/views/sys/login/Login.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="login">
-    <div>这是测试</div>
+    <div>这是测试s</div>
     <div>{{ msg }}</div>
   </div>
 </template>
 
 <script lang="ts">
-  import { defineComponent } from 'vue';
-  export default defineComponent({
-    props: {
-      msg: {
-        type: String,
-        required: true,
-      },
+import { defineComponent } from 'vue';
+export default defineComponent({
+  props: {
+    msg: {
+      type: String,
+      required: true,
     },
-    setup() {},
-  });
+  },
+  setup() {},
+});
 </script>
